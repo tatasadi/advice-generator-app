486cad9b85424d3a18550f7ceb0e589581c9488c
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-02-07 05:57:07 +0100

    init project

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "light-cyan": "hsl(193, 38%, 86%)",
+        "neon-green": "hsl(150, 100%, 66%)",
+        "grayish-blue": "hsl(217, 19%, 38%)",
+        "dark-grayish-blue": "hsl(217, 19%, 24%)",
+        "dark-blue": "hsl(218, 23%, 16%)",
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
